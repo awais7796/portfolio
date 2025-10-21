b3f835cba078b132a069a39f3f16613c49f0da96
awais7796 <awaitzz49@gmail.com>
2025-10-21 18:16:26 +0530

    importeted  Button

--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -1,7 +1,12 @@
 import React from "react";
-
+import { Button } from "@/components/ui/stateful-button";
 const Contact = () => {
-  return <div className=" pt-10 text-border">Contact us</div>;
+  return (
+    <div className="flex flex-col pt-10 text-border">
+      Contact us
+      <Button>Submit</Button>
+    </div>
+  );
 };
 
 export default Contact;
