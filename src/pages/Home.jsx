1c53b7437dc1bbad203e7013f87ccb7ab3aef108
awais7796 <awaitzz49@gmail.com>
2025-10-21 17:55:32 +0530

    developed the scartch design of home

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,6 +1,23 @@
 import React from "react";
+import { ShimmerButton } from "@/components/ui/shimmer-button";
 const Home = () => {
-  return <div>Home</div>;
+  return (
+    <div className="  h-screen pt-5 flex flex-col justify-center items-center  gap-5">
+      <div>
+        <a href="d">leek here </a>
+      </div>
+      <div>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptatibus
+        animi deserunt explicabo facilis nemo, voluptatem consequatur
+        dignissimos est similique tenetur dolorum officiis. Aspernatur minima
+        quisquam repellat architecto dolores vitae adipisci.
+      </div>
+      <div className="flex justify-center items-center gap-5">
+        <ShimmerButton>Book a meet </ShimmerButton>
+        <ShimmerButton>get in touch </ShimmerButton>
+      </div>
+    </div>
+  );
 };
 
 export default Home;
