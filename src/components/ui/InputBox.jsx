74ba3c4b485a1a2a1963981787ac03a1a7742a82
awais7796 <awaitzz49@gmail.com>
2025-11-02 15:49:52 +0530

    minor changes in button and removes useless css properties

--- src/components/ui/InputBox.jsx
+++ src/components/ui/InputBox.jsx
@@ -1,9 +1,10 @@
-const InputBox = ({ placeholder }) => {
+const InputBox = ({ label, placeholder }) => {
   return (
     <>
-      <div className="flex flex-col ">
+      <div className=" flex flex-col items-start  w-full pl-1.5 mb-4">
+        <label className="text-sm font-medium mb-1">{label}</label>
         <input
-          className="border border-gray-200 rounded-xl p-3 focus:border-indigo-300 focus:ring focus:ring-indigo-100 outline-none transition"
+          className="w-full  border border-gray-200 rounded-xl p-3 focus:border-indigo-300 focus:ring focus:ring-indigo-100 outline-none transition"
           type="text"
           placeholder={placeholder}
         />
