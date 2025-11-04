300364a4409b4b8259f2f56657d6732257aa0f4e
awais7796 <awaitzz49@gmail.com>
2025-11-04 17:13:22 +0530

    added props for cal.com into buttons compt

--- src/components/ui/button.jsx
+++ src/components/ui/button.jsx
@@ -1,12 +1,16 @@
 // import { Plus } from "lucide-react";
 
-const Button = ({ url, label, SvgIcon }) => {
+const Button = ({ url, label, SvgIcon, ...props }) => {
   const handleClick = () => {
-    if (url) window.open(url, "_blank");
+    if (url) {
+      e.preventDefault();
+      window.open(url, "_blank");
+    }
   };
   return (
     <>
       <button
+        {...props}
         onClick={handleClick}
         className=" flex gap-2 border-2 w-auto p-3 m-3 justify-center items-center rounded-md"
       >
