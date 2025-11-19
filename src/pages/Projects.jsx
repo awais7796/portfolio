f3db60bf46d1a8b96597bab526d6c1ff875993d9
awais7796 <awaitzz49@gmail.com>
2025-11-20 00:08:08 +0530

    removed readme and changes in project.jsx

--- src/pages/Projects.jsx
+++ src/pages/Projects.jsx
@@ -1,20 +1,24 @@
 import { a } from "framer-motion/dist/types.d-BJcRxCew";
 
-const Projects = () => {]
-  const projects =[{
-    "title" :"Ghibbli art genertaor ",
-    "description":"it genrated the ghibli art for the input img "
-  },{
-    "title" :"Healthcare recommendation sysytem",
-    "description":"give health recommendtaion based on th esymtopms that user provodes"
-  },{
-    "title" :"MansTalk ai",
-    "description":"chat based ai which answer mans queries about there sexual health "
-  }]
+const Projects = () => {
+  const projects = [
+    {
+      title: "Ghibbli art genertaor ",
+      description: "it genrated the ghibli art for the input img ",
+    },
+    {
+      title: "Healthcare recommendation sysytem",
+      description:
+        "give health recommendtaion based on th esymtopms that user provodes",
+    },
+    {
+      title: "MansTalk ai",
+      description:
+        "chat based ai which answer mans queries about there sexual health ",
+    },
+  ];
 
-
-  return <div id="projects" className="h-screen">projects
-  </div>;
+  return projects.map(() => {});
 };
 
 export default Projects;
